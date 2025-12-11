01df96d38222199c497643e72ba304bd7011b37f
APostolit <50382487+APostolit@users.noreply.github.com>
2025-12-11 13:55:13 +0000

    Add files via upload

---
+++ pages/glava_9/Listing_9_1.py
@@ -0,0 +1,15 @@
+# Листинг 9.1
+print('Начальные значения переменным x и y')
+x = 10
+y = 20
+print('x =', x, 'y =', y)
+
+print('Сравниваем числа')
+print('x == 10 ->', x == 10)
+print('x != 10 ->', x != 10)
+print('x > y ->', x > y)
+print('x < y ->', x < y)
+print('x >= 10 ->', x >= 10)
+print('y <= 20 ->', y <= 20)
+print('True = 1 ->', True == 1)
+print('False = 0 ->', False == 0)
